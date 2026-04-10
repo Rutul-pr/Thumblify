62580311dd92e10283da131ed8fc95a86925d1ae
RutulPrajapati <rutulp79@gmail.com>
2026-04-10 09:06:37 +0530

    url added in server.ts at middlewares cors at origin

--- server/server.ts
+++ server/server.ts
@@ -23,7 +23,7 @@ const app = express();
 
 // Middlewares
 app.use(cors({
-    origin:['http://localhost:5173','http://localhost:3000'],
+    origin:['http://localhost:5173','http://localhost:3000',"https://thumblify-rutul.vercel.app"],
     credentials:true,
 }))
 
